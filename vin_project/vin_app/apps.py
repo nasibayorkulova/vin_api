cb240a4f1ea7f11104685df9d70fb4eb9dd1a680
nasibayorkulova <78695171+nasibayorkulova@users.noreply.github.com>
2021-09-12 19:22:43 +0500

    first commit: project created. models migrated

---
+++ vin_project/vin_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class VinAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'vin_app'
---
+++ vin_project/vin_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+
+# Create model VIN to save vehicle details.
+class VIN(models.Model):
+    vin = models.CharField(max_length=17)
+    year = models.CharField(max_length=4)
+    make = models.CharField(max_length=30)
+    model = models.CharField(max_length=30)
+    type = models.CharField(max_length=30)
+    color = models.CharField(max_length=15)
+    dimensions = models.CharField(max_length=100)
+    weight = models.CharField(max_length=10)
